703df25e96c25b83d73184d3ebc00082c359e11b
Elena <aleklenoch@gmail.com>
2023-07-22 00:21:29 +0300

    #Task17 Добавлено дополнительное окно Form2.
    необходимо для ввода данных пользователем

---
+++ Git_NoteBook/Form2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Git_NoteBook
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
