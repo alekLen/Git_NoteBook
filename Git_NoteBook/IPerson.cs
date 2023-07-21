797084a6ebe09c72c7a9256ba7674de3bfc0f106
Alex <aleklenoch@gmail.com>
2023-07-21 22:11:23 +0300

    #Task1 Добавление интерфейса IPerson.
    В файл IPerson.cs добавлен интерфейс IPerson.
    является частью модели.
    Обязывает будующий класс Person реализовать
    3 свойства и 1 метод.

---
+++ Git_NoteBook/IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_NoteBook
+{
+    internal interface IPerson
+    {
+        string Name { get; set; }
+        string Phone { get; set; }
+        string Adress { get; set; }
+        string PersonToString();
+    }
+}
