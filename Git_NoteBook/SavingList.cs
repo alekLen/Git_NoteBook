de3fb4416880f7ea40b2a3afb30c617484089955
Alex <aleklenoch@gmail.com>
2023-07-21 22:20:48 +0300

    #Task5 Создан класс SavingList.
    В файле SavingList.cs создан класс SavingList.
    Класс содержит коллекцию ссылок на класс Person.
    Будет хранить список людей и их данные.

---
+++ Git_NoteBook/SavingList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_NoteBook
+{
+    internal class SavingList
+    {
+        public List<IPerson> list = new List<IPerson>();
+    }
+}
