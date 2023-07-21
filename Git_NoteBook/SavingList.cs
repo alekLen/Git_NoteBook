f8602f7eb0acfc1cf78e4451da15380908ce27ad
Alex <aleklenoch@gmail.com>
2023-07-21 22:24:34 +0300

    #Task7 Добавление классу SavingList  метода Save(string filename) и два свойства.
    Позволяет сохранить с помощью сериализации
    список контактов в файл.
    Метод принимает название файла для сохранения.

--- Git_NoteBook/SavingList.cs
+++ Git_NoteBook/SavingList.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 namespace Git_NoteBook
 {
     internal class SavingList
     {
+
+        FileStream stream = null;
+        BinaryFormatter formatter = null;
         public List<IPerson> list = new List<IPerson>();
+        public void Save(string filename)
+        {
+            stream = new FileStream(filename, FileMode.OpenOrCreate);
+            formatter = new BinaryFormatter();
+            formatter.Serialize(stream, list);
+            stream.Close();
+        }
     }
 }
