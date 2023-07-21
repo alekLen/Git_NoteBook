5d5f970418b66d2d6ef289cb98b50e6b34941c8d
Alex <aleklenoch@gmail.com>
2023-07-21 22:14:02 +0300

    #Task2 Создание класса Person.
    Класс Person реализует интерфейс IPerson.
    Описывает сущность person.
    Имеет свойства Name, Adress, Phone.

---
+++ Git_NoteBook/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_NoteBook
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+    }
+}
