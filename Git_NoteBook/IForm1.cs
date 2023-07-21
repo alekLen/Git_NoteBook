a2160a04c448fb404d59e98260be928742bcb20c
Alex <aleklenoch@gmail.com>
2023-07-21 22:28:59 +0300

    #Task9 Создан интерфейс IForm1.
    содержит свойства Name, Adress, Phone.
    является частью представления.
    нужен для обращения к форме через ссылку.

---
+++ Git_NoteBook/IForm1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_NoteBook
+{
+    internal interface IForm1
+    {
+        string NamePerson { get; set; }
+        string Phone { get; set; }
+        string Adress { get; set; }
+    }
+}
