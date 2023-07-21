f3560cd9940645b7f6572f2c73713313592ef119
Alex <aleklenoch@gmail.com>
2023-07-21 22:31:03 +0300

    #Task10 Создан класс ListOfMan
    В файле ListOfMan.cs создан класс ListOfMan.
    класс служит посредником между моделью
    и представлением в рамках MVP.
    имеет два свойства: IForm1  и SavingList.

---
+++ Git_NoteBook/ListOfMan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_NoteBook
+{
+    internal class ListOfMan
+    {
+        private readonly IForm1 form;
+
+        private readonly SavingList save;
+    }
+}
