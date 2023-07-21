26f0760bcacf9506e540954eea9f1aa7df1975f2
Alex <aleklenoch@gmail.com>
2023-07-21 23:26:50 +0300

    #Task12 изменен класс Form1.
    Добавлены свойства NamePerson, Phone, Adress.
    необходимы для работы с данными введенными пользователем.

--- Git_NoteBook/Form1.cs
+++ Git_NoteBook/Form1.cs
@@ -10,6 +10,9 @@ namespace Git_NoteBook
 {
     public partial class Form1 : Form
     {
+        public string NamePerson { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
         public Form1()
         {
             InitializeComponent();
