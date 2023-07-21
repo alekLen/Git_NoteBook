b3bf7f3cf24bee18e38039dc798be3295dff7c69
Alex <aleklenoch@gmail.com>
2023-07-21 22:22:46 +0300

    #Task6 Добавление интерфейса ISave
    В файл ISave.cs добавлен интерфейс ISave.
    Обязывает реализовать 2 метода:
     Save(string filename) и Load(string filename).

---
+++ Git_NoteBook/ISave.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git_NoteBook
+{
+    internal interface ISave
+    {
+        void Save(string filename);
+        void Load(string filename);
+    }
+}
